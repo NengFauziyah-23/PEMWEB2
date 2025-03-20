1f3b0e0d20cd1fe1df88eac16639190c3a1ceb03
Your Name <nnengfauziyahhasanah@gmail.com>
2025-03-20 23:05:32 +0700

    upload file04

---
+++ pertemuan4/lingkaran.php
@@ -0,0 +1,29 @@
+<?php
+class lingkaran{
+    public $jari;
+    public const PHI = 3.14;
+
+    /**
+     * konstanta untuk menampung nilai PHI
+     */
+    public function __construct($r){
+    $this->jari = $r;
+    }
+
+    public function getluas(){
+        $luas = self::PHI * $this->jari * $this->jari;
+        return $luas;
+    }
+
+    public function getkeliling(){
+        $keliling = 2.0 * self::PHI * $this->jari;
+        return $keliling;
+    }
+    
+    public function cetak(){
+        echo "Lingkaran dengan jari2 " . $this->jari;
+        echo "<br>Luas" . $this->getluas();
+        echo "<br>Keliling" . $this->getkeliling();
+    }
+}
+?>
